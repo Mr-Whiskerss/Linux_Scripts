4ee6053fef5ea9b7eded38b53ecf4a9299cc3ac4
MrWhiskers <42265828+Mr-Whiskerss@users.noreply.github.com>
2024-07-15 01:16:14 +0100

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -1,115 +1,105 @@
 #!/bin/bash
-#This script is designed to pull down my most used tools to quickly build out my linux system
+# This script is designed to pull down my most used tools to quickly build out my Linux system
 
-echo Welcome to the Build Script
+echo "Welcome to the Build Script"
 
-#Checking if script is running at root.
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+# Checking if script is running as root.
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
 fi
-echo Root Checking done 
+echo "Root check passed"
 
-
-#Update repo and upgrade system first.
-
-# Update
+# Update and upgrade the system
 echo "=================================="
-echo "Update"
+echo "Updating and Upgrading System"
 echo "=================================="
-sudo apt-get update
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt autoremove -y
 
-
-# Upgrade
+# Install common tools
 echo "=================================="
-echo "Upgrade"
+echo "Installing Common Tools"
 echo "=================================="
-sudo apt-get upgrade
+sudo apt-get install -y htop iftop python3-pip python2 seclists terminator rubygems
 
-#Remove old software 
-echo "=================================="
-echo "Remove old software"
-echo "=================================="
-sudo apt autoremove
-
-# Distribution Upgrade
-echo "=================================="
-echo "Distribution Upgrade"
-echo "=================================="
-sudo apt-get dist-upgrade
-
-#The following commands will install common tools used within my personal kali install. More will be updated as I progress (2024)
-#Install common global tools please feel free to add or remove tools from here this is personal preference
-echo "=================================="
-echo "Installing Software"
-echo "=================================="
-apt-get install htop iftop python3-pip python2 seclists terminator rubygems -y 
-
-
-#create new tools directory and report directory for kali on first boot
-echo Creating Tool directories
-mkdir /opt/tools
-
-
-
-#cd into new directory 
+# Create tools directory
+echo "Creating tool directories"
+mkdir -p /opt/tools
 cd /opt/tools
 
+# Install pentest tools
 echo "=================================="
 echo "Installing Pentest Tools"
 echo "=================================="
-#Enum4linux-ng
-echo Installing Enum4linix-NG
-apt-get install smbclient python3-ldap3 python3-yaml python3-impacket -y
+
+# Enum4linux-ng
+echo "Installing Enum4linux-NG"
+sudo apt-get install -y smbclient python3-ldap3 python3-yaml python3-impacket
 git clone https://github.com/cddmp/enum4linux-ng.git
 
-#testssl
+# testssl
+echo "Installing testssl"
 git clone --depth 1 https://github.com/drwetter/testssl.sh.git
 
-#Powersploit 
+# PowerSploit
+echo "Installing PowerSploit"
 git clone https://github.com/PowerShellMafia/PowerSploit.git
- 
-#Impacket
+
+# Impacket
+echo "Installing Impacket"
 git clone https://github.com/CoreSecurity/impacket.git
-python3 opt/tools/impacket/setup.py install
- 
-#Winpeas and Linpeas
+cd impacket
+python3 setup.py install
+cd ..
+
+# WinPEAS and LinPEAS
+echo "Installing WinPEAS and LinPEAS"
 wget https://github.com/peass-ng/PEASS-ng/releases/download/20240714-cd435bb2/linpeas.sh
 wget https://github.com/peass-ng/PEASS-ng/releases/download/20240714-cd435bb2/winPEASx64.exe
 wget https://github.com/peass-ng/PEASS-ng/releases/download/20240714-cd435bb2/winPEASx86.exe
 
-
-#Linenum 
+# LinEnum
+echo "Installing LinEnum"
 git clone https://github.com/rebootuser/LinEnum.git
 
-#Responder
+# Responder
+echo "Installing Responder"
 git clone https://github.com/lgandx/Responder.git
 
-#dnscan
+# dnscan
+echo "Installing dnscan"
 git clone https://github.com/rbsec/dnscan.git
-pip3 install -r opt/tools/dnscan/requirements.txt
+pip3 install -r dnscan/requirements.txt
 
-#winrm
+# evil-winrm
+echo "Installing evil-winrm"
 gem install evil-winrm
 
-#wifite (For wireless testing)
-git clone https://github.com/kimocoder/wifite2.git 
-pip install -r opt/tools/wifite2/requirements.txt
+# Wifite2
+echo "Installing Wifite2"
+git clone https://github.com/kimocoder/wifite2.git
+pip3 install -r wifite2/requirements.txt
 
-#AutoRecon
+# AutoRecon
+echo "Installing AutoRecon"
 python3 -m pip install git+https://github.com/Tib3rius/AutoRecon.git
 
-#NetExce
-sudo apt install crackmapexec
+# CrackMapExec
+echo "Installing CrackMapExec"
+sudo apt install -y crackmapexec
 
-#SecLists
+# SecLists
+echo "Installing SecLists"
 git clone https://github.com/danielmiessler/SecLists.git
 
-#Fierce
-apt install fierce
+# Fierce
+echo "Installing Fierce"
+sudo apt install -y fierce
 
-#FinalWebRecon
+# FinalRecon
+echo "Installing FinalRecon"
 git clone https://github.com/thewhiteh4t/FinalRecon.git
 cd FinalRecon
 pip3 install -r requirements.txt
 
+echo "Build Script Complete"
